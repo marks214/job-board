5eceb728f69f648889dc272791a6fb9030bf8ed9
marks214 <aimee.oz.x@gmail.com>
2020-11-01 11:26:06 -0800

    linked show page from the index page

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,6 +2,18 @@ class JobsController < ApplicationController
 
   def index
     # @jobs (instance variable) gives us access to the jobs outside the controller
+    # -in this case, index.html.erb can access @jobs
     @jobs = Job.all
   end
+
+  def show
+    @job = Job.find(params[:id])
+
+    if @job.nil?
+      redirect_to '/jobs'
+      return
+    end
+  end
+
 end
+
