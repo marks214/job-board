705e540d88cff8a30f5e93595afab8e25cefdbf2
marks214 <aimee.oz.x@gmail.com>
2020-11-01 11:06:13 -0800

    added list of jobs to the index action and view

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,7 @@
 class JobsController < ApplicationController
 
   def index
-    
+    # @jobs (instance variable) gives us access to the jobs outside the controller
+    @jobs = Job.all
   end
 end
